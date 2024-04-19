f1f34c2e771dc417ca9f1a8b60cda573462e4f8e
PhucQuach1907 <phucquach1907@gmail.com>
2024-04-19 10:44:41 +0700

    Fix: Change logic login

--- user/login/views.py
+++ user/login/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import json
 
 
 @api_view(['POST'])
@@ -17,7 +18,8 @@ def login(request):
     if not user:
         return Response({'error': 'Email hoặc mật khẩu không đúng'}, status=status.HTTP_401_UNAUTHORIZED)
 
-    token, created = Token.objects.get_or_create(user=user)
-    response = Response({'token': token.key, 'user_id': user.pk, 'email': user.email}, status=status.HTTP_200_OK)
-    response.set_cookie('token', token.key, httponly=True, max_age=3600)
+    response = Response({'user_id': user.pk, 'is_staff': user.is_staff}, status=status.HTTP_200_OK)
+    cookie_data = {'user_id': user.pk, 'is_staff': user.is_staff}
+    cookie_string = json.dumps(cookie_data)
+    response.set_cookie('user_info', cookie_string, httponly=True)
     return response
