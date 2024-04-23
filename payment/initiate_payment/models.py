c08683c9f026ddc8b812cc2154d4bb0de4ab2514
PhucQuach1907 <phucquach1907@gmail.com>
2024-04-23 10:36:17 +0700

    Feat: create model payment

--- payment/initiate_payment/models.py
+++ payment/initiate_payment/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.apps import apps
+
 
 # Create your models here.
+class Payment(models.Model):
+    user = models.ForeignKey(apps.get_model(app_label='user.user', model_name='User'), on_delete=models.CASCADE)
+    cart_items = models.ManyToManyField(apps.get_model(app_label='cart.cart_item', model_name='CartItem'))
+    total_price = models.BigIntegerField()
+    payment_mode = models.CharField(max_length=50)
+
+    def calculate_total_price(self):
+        total_price = 0
+        for item in self.cart_items.all():
+            total_price += item.subtotal
+        self.total_price = total_price
+        self.save()
+
+    def save(self, *args, **kwargs):
+        self.calculate_total_price()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Payment {self.pk} by {self.user}"
--- payment/payment/settings.py
+++ payment/payment/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'initiate_payment',
+    'payment_status'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'payment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecomsys-payment',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
