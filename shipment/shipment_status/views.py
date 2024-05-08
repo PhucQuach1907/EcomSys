a838c0b6754d5095bcf36ed7219c5a8946fdaf81
PhucQuach1907 <phucquach1907@gmail.com>
2024-05-08 12:28:11 +0700

    feat: create BE for shipment service

--- shipment/shipment_status/views.py
+++ shipment/shipment_status/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import ShipmentStatus
+from .serializers import ShipmentStatusSerializer
+
 
 # Create your views here.
+class ShipmentStatusViewSet(viewsets.ModelViewSet):
+    queryset = ShipmentStatus.objects.all()
+    serializer_class = ShipmentStatusSerializer
--- shipment/shipment_updates/models.py
+++ shipment/shipment_updates/models.py
@@ -1,3 +1,14 @@
+from django.apps import apps
 from django.db import models
 
+
 # Create your models here.
+class Shipment(models.Model):
+    payment_status_id = models.PositiveSmallIntegerField()
+    client_name = models.CharField(max_length=255)
+    mobile_number = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'shipment_updates'
