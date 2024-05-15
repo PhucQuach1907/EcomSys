c5f43e986f218e8326b284d9ea2b2b539c9294ec
PhucQuach1907 <phucquach1907@gmail.com>
2024-05-15 11:06:52 +0700

    feat: create payment QR

--- templates/static/js/checkout.js
+++ templates/static/js/checkout.js
@@ -2,6 +2,14 @@ let cart_items = [];
 let total = 0;
 let products = [];
 let product_types = [];
+let my_bank = {
+    BANK_ID: "vietinbank",
+    ACCOUNT_NO: "104871920702",
+    TEMPLATE: "compact2",
+    AMOUNT: total,
+    DESCRIPTION: "THANH TOAN DON HANG",
+    ACCOUNT_NAME: "EcomSys"
+}
 document.addEventListener('DOMContentLoaded', function () {
     const token = sessionStorage.getItem('token');
     if (!token) {
@@ -96,6 +104,7 @@ function displayProducts(items) {
                 productPrice.textContent = `Subtotal: ${price} VND`;
                 total += item.quantity * product.price;
                 totalParagraph.textContent = `Total: ${formatPrice(total)} VND`;
+                my_bank.AMOUNT = total;
             });
         contentWrapper.appendChild(productName);
         contentWrapper.appendChild(productQuantity);
@@ -113,7 +122,7 @@ function handleCheckout(name, mobileNumber, address) {
     popup.classList.add('fixed', 'top-0', 'left-0', 'w-full', 'h-full', 'bg-black', 'bg-opacity-50', 'flex', 'justify-center', 'items-center', 'z-50');
 
     const popupContent = document.createElement('div');
-    popupContent.classList.add('relative', 'flex', 'flex-col', 'bg-white', 'p-4', 'rounded-lg', 'shadow-lg', 'w-3/5', 'h-3/5', 'justify-center');
+    popupContent.classList.add('relative', 'flex', 'flex-col', 'bg-white', 'p-4', 'rounded-lg', 'shadow-lg', 'w-3/5', 'h-fit', 'justify-center');
 
     const qrMessage = document.createElement('div');
     qrMessage.classList.add('text-center', 'text-gray-400', 'text-2xl', 'font-semibold', 'mb-2');
@@ -121,8 +130,8 @@ function handleCheckout(name, mobileNumber, address) {
     popupContent.appendChild(qrMessage);
 
     const qrCode = document.createElement('img');
-    qrCode.classList.add('self-center', 'w-52', 'h52');
-    qrCode.src = 'https://upload.wikimedia.org/wikipedia/commons/2/2f/Rickrolling_QR_code.png?20200615212723';
+    qrCode.classList.add('self-center', 'w-3/5', 'h-3/5');
+    qrCode.src = `https://img.vietqr.io/image/${my_bank.BANK_ID}-${my_bank.ACCOUNT_NO}-${my_bank.TEMPLATE}.png?amount=${my_bank.AMOUNT}&addInfo=${my_bank.DESCRIPTION}&accountName=${my_bank.ACCOUNT_NAME}`;
     popupContent.appendChild(qrCode);
 
     const closeButton = document.createElement('button');
@@ -133,25 +142,12 @@ function handleCheckout(name, mobileNumber, address) {
     });
     popupContent.appendChild(closeButton);
 
-    const doneButton = document.createElement('button');
-    doneButton.textContent = 'DONE';
-    doneButton.classList.add('absolute', 'bottom-0', 'right-0', 'w-60', 'm-3', 'py-2', 'bg-pink-500', 'text-white', 'uppercase', 'font-semibold', 'tracking-wider', 'hover:bg-pink-600', 'focus:outline-none', 'focus:bg-pink-600', 'transition', 'duration-300', 'ease-in-out', 'rounded-lg');
-    doneButton.addEventListener('click', () => {
-        initPayment()
-            .then(response => {
-                return response.json().then(data =>
-                    updatePaymentStatus(data.id)
-                        .then(response => handleUpdatePaymentStatusResponse(response, data, name, mobileNumber, address))
-                        .catch(error => console.error('There was a problem with your fetch operation:', error))
-                )
-            })
-            .catch(error => console.error('There was a problem with your fetch operation:', error));
-    });
-    popupContent.appendChild(closeButton);
-    popupContent.appendChild(doneButton);
-
     popup.appendChild(popupContent);
     document.body.appendChild(popup);
+
+    setInterval(() => {
+        checkPaid(name, mobileNumber, address);
+    }, 1000);
 }
 
 function takeShipmentInfo() {
@@ -215,6 +211,7 @@ function takeShipmentInfo() {
         const name = nameInput.value;
         const mobileNumber = mobileInput.value;
         const address = addressInput.value;
+        my_bank.DESCRIPTION = my_bank.DESCRIPTION + ` CUA ${mobileNumber}`;
         handleCheckout(name, mobileNumber, address);
     });
     buttonContainer.appendChild(submitButton);
@@ -324,5 +321,28 @@ function handleUpdatePaymentStatusResponse(response, data, name, mobileNumber, a
             console.log(data.detail);
             throw new Error('Failed to update payment status.');
         });
+    }
+}
+
+async function checkPaid(name, mobileNumber, address) {
+    try {
+        const response = await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=5wCXwSzx8Tv5wyIsC7PWlFiUjhu9YE5otQDcXGSjaMURRxooBJVuwS5dhTMYstKQvRWkGli2TWEJopt3bzKu76x5GfbBF7pBm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOYldl1fOWGaAP8x2Mrc0DMXgeiy1KGB6Q8xOkU-8nDDuDOw5r98iK2JetQy4zkebaugHKbfLwzIR0Q4TmKN3_vz0ib1IC3sm9z9Jw9Md8uu&lib=MGGgpe8YTQe5KoH6LauzIyHcXGD38jLy9");
+        const data = await response.json();
+        const lastPaid = data.data[data.data.length - 1];
+        if (lastPaid["Giá trị"] >= my_bank.AMOUNT && lastPaid["Mô tả"].includes(my_bank.DESCRIPTION)) {
+            console.log("success!");
+            initPayment()
+            .then(response => {
+                return response.json().then(data =>
+                    updatePaymentStatus(data.id)
+                        .then(response => handleUpdatePaymentStatusResponse(response, data, name, mobileNumber, address))
+                        .catch(error => console.error('There was a problem with your fetch operation:', error))
+                )
+            })
+            .catch(error => console.error('There was a problem with your fetch operation:', error));
+        }
+    }
+    catch {
+        console.error("Có lỗi xảy ra!");
     }
 }
